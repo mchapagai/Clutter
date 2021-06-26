bc197567458f4ae00b70b5bc74fa6fc6f4227a46
“Manorath <mono.chapagai@gmail.com>
2021-06-26 16:46:52 -0400

    initial commit

---
+++ android/app/src/main/kotlin/com/example/clutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/env/main.dart
@@ -0,0 +1,7 @@
+import 'package:clutter/application/clutter_application.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ClutterApplication());
+}
+
